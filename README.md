d17db68c1897fa3205159fee4c5da721aaa3d1ec
Rasmus Kaj <kaj@kth.se>
2016-07-15 15:07:07 +0200

    Start work on actual upload.

--- README.md
+++ README.md
@@ -6,4 +6,20 @@ In social, with production server configuration (i.e. access to production datab
 
     ./manage.py dump_course_contents $COURSE_CODE
 
+Prepare for running this project as for any python 3 project, something like this:
+
+    virtualenv-3.5 venv
+    . ./venv/bin/activate
+    pip install -r requirements.txt
+
+Then, in this project, with the dump from above in a directory called
+`dump`, and with the folloing information known:
+
+$COURSE_ID: A numeric course id from canvas
+$COURSE_CODE: The code used by KTH for course, something like AB1234.
+$MODULE: (TODO, I don't know what this is supposed to be, it is currently unused)
+$SLUG: The file name (sans .html) of the file to upload from dump.
+
+    ./src/import_page.py -v $COURSE_ID $COURSE_CODE $MODULE $SLUG
+
 This is a work in progress.
---
+++ src/import_page.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+import optparse
+import requests
+from bs4 import BeautifulSoup
+from json import load as parse_json
+
+with open('config.json') as json_data_file:
+    configuration = parse_json(json_data_file)
+    canvas = configuration['canvas']
+    access_token= canvas["access_token"]
+    baseUrl = 'https://%s/api/v1/courses/' % canvas.get('host', 'kth.instructure.com')
+    header = {'Authorization' : 'Bearer ' + access_token}
+
+def main():
+    parser = optparse.OptionParser(
+        usage="Usage: %prog [options] course_id course_code module filename")
+
+    parser.add_option('-v', '--verbose',
+                      dest="verbose",
+                      default=False,
+                      action="store_true",
+                      help="Print lots of output to stdout"
+    )
+    
+    options, args = parser.parse_args()
+    if len(args) != 4:
+        parser.error("course_id, course_code, module, and filename are required")
+    course_id, course_code, module, filename = args
+    if options.verbose:
+        print("Upload", filename, "to", course_id, course_code, module)
+
+    with open('dump/%s/pages.json' % course_code) as json:
+        data = parse_json(json)
+    data = next(filter(lambda i: i['slug'] == filename, data), None)
+    if not data:
+        print("Page", filename, "to upload not found in dump")
+        exit(1)
+    elif options.verbose:
+        print("Should upload", data)
+
+    # Use the Canvas API to insert the page
+    #PUT /api/v1/courses/:course_id/pages
+    #    wiki_page[title]
+    #    wiki_page[body]
+    #    wiki_page[published]
+    html = BeautifulSoup(open("dump/%s/pages/%s.html" % (course_code, data['slug'])), "html.parser")
+    url = baseUrl + '%s/pages' % (course_id)
+    print("Should post page to", url)
+    payload={
+        'wiki_page[title]': data['title'],
+        'wiki_page[published]': False,
+        'wiki_page[body]': str(html)
+    }
+    if options.verbose:
+        print(payload)
+    r = requests.post(url, headers = header, data=payload)
+    print("result of post creating page: " + r.text)
+    if r.status_code == requests.codes.ok:
+        page_response=r.json()
+
+
+if __name__ == '__main__': main()
