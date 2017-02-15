b2f1ace392e9875ca0ce875a616dda4e0bd873d0
Rasmus Kaj <kaj@kth.se>
2017-02-15 13:29:31 +0100

    Handle multiple courses in one run.

--- src/import_course.py
+++ src/import_course.py
@@ -35,98 +35,106 @@ def main():
     )
 
     options, args = parser.parse_args()
-    if len(args) != 1:
-        parser.error("course_code is required")
-    course_code, = args
-    course_id = options.canvasid or find_canvas_id(course_code)
-    if not course_id:
-        print("Canvas course id not given or found")
-        exit(1)
-    dumpdir = options.dumpdir
-    if options.verbose:
-        print("Upload to %s (canvas #%s) from %s" % (
-            course_code, course_id, dumpdir))
+    if options.canvasid and len(args) != 1:
+        parser.error("Exactly one course_code is required when giving canvas id")
+    elif len(args) == 0:
+        parser.error("At least one course_code is required")
 
-    course_code = course_code[:6]
-    with open('%s/%s/pages.json' % (dumpdir, course_code)) as json:
-        dumpdata = parse_json(json)
-
-    uploaded_files = {}
-    for data in dumpdata:
+    for course_code in args:
+        course_id = options.canvasid or find_canvas_id(course_code)
+        if not course_id:
+            print("Canvas course id not given or found")
+            exit(1)
+        dumpdir = options.dumpdir
         if options.verbose:
-            print("Should upload", data)
+            print("Upload to %s (canvas #%s) from %s" % (
+                course_code, course_id, dumpdir))
 
-        # Use the Canvas API to insert the page
-        #POST /api/v1/courses/:course_id/pages
-        #    wiki_page[title]
-        #    wiki_page[body]
-        #    wiki_page[published]
-        html = BeautifulSoup(open("%s/%s/pages/%s.html" % (dumpdir, course_code, data['slug'])), "html.parser")
-        for link in html.findAll(href=True):
-            linkdata = next(filter(lambda i: i['url'] == link['href'], data['links']), None)
-            if linkdata['category'] == 'file':
-                canvas_url = uploaded_files.get(link['href'])
-                if not canvas_url:
-                    canvas_url = create_file(course_id, '%s/%s/pages/%s' % (dumpdir, course_code, linkdata['url']),
-                                             basename(linkdata['url']))
-                    print("Uploaded %s to %s for link" % (link['href'], canvas_url))
-                    uploaded_files[link['href']] = canvas_url
-                else:
-                    print("%s is allready at %s" % (link['href'], canvas_url))
-                link['href'] = canvas_url
-                linkdata['url'] = canvas_url
+        course_code = course_code[:6]
+        with open('%s/%s/pages.json' % (dumpdir, course_code)) as json:
+            dumpdata = parse_json(json)
 
-        for img in html.findAll('img'):
-            imgdata = next(filter(lambda i: i['url'] == img['src'], data['links']), None)
-            if imgdata['category'] == 'file':
-                canvas_url = uploaded_files.get(img['src'])
-                if not canvas_url:
-                    canvas_url = create_file(course_id, '%s/%s/pages/%s' % (dumpdir, course_code, imgdata['url']),
-                                             basename(imgdata['url']))
-                    print("Uploaded %s to %s for img" % (img['src'], canvas_url))
-                    uploaded_files[img['src']] = canvas_url
-                else:
-                    print("%s is allready at %s" % (img['src'], canvas_url))
-                img['src'] = canvas_url
-                imgdata['url'] = canvas_url
-
-        for tex in html.findAll('span', attrs={'role': 'formula', 'data-language': 'tex'}):
-            img = html.new_tag('img')
-            img['src'] = '/equation_images/' + urlquote(tex.text)
-            img['alt'] = tex.text
-            img['class'] = tex.get('class')
-            tex.replace_with(img)
+        uploaded_files = {}
+        for data in dumpdata:
             if options.verbose:
-                print("Modified formula %s to: %s" % (tex, img))
+                print("Should upload", data)
 
-        url = baseUrl + '%s/pages' % (course_id)
-        print("Should post page to", url)
-        payload={
-            'wiki_page[title]': data['title'],
-            'wiki_page[published]': False,
-            'wiki_page[body]': str(html)
-        }
-        if options.verbose:
-            print(payload)
-        r = requests.post(url, headers = header, data=payload)
-        if r.status_code == requests.codes.ok:
-            page_response=r.json()
+            # Use the Canvas API to insert the page
+            #POST /api/v1/courses/:course_id/pages
+            #    wiki_page[title]
+            #    wiki_page[body]
+            #    wiki_page[published]
+            html = BeautifulSoup(open("%s/%s/pages/%s.html" % (dumpdir, course_code, data['slug'])), "html.parser")
+            for link in html.findAll(href=True):
+                linkdata = next(filter(lambda i: i['url'] == link['href'], data['links']), None)
+                if linkdata and linkdata['category'] == 'file':
+                    canvas_url = uploaded_files.get(link['href'])
+                    if not canvas_url:
+                        canvas_url = create_file(course_id, '%s/%s/pages/%s' % (dumpdir, course_code, linkdata['url']),
+                                                 basename(linkdata['url']))
+                        print("Uploaded %s to %s for link" % (link['href'], canvas_url))
+                        uploaded_files[link['href']] = canvas_url
+                    else:
+                        print("%s is allready at %s" % (link['href'], canvas_url))
+                    link['href'] = canvas_url
+                    linkdata['url'] = canvas_url
+
+            for img in html.findAll('img'):
+                imgdata = next(filter(lambda i: i['url'] == img.get('src'), data['links']), {})
+                if imgdata.get('category') == 'file':
+                    canvas_url = uploaded_files.get(img['src'])
+                    if not canvas_url:
+                        canvas_url = create_file(course_id, '%s/%s/pages/%s' % (dumpdir, course_code, imgdata['url']),
+                                                 basename(imgdata['url']))
+                        print("Uploaded %s to %s for img" % (img['src'], canvas_url))
+                        uploaded_files[img['src']] = canvas_url
+                    else:
+                        print("%s is allready at %s" % (img['src'], canvas_url))
+                    img['src'] = canvas_url
+                    imgdata['url'] = canvas_url
+
+            for tex in html.findAll('span', attrs={'role': 'formula', 'data-language': 'tex'}):
+                img = html.new_tag('img')
+                img['src'] = '/equation_images/' + urlquote(tex.text)
+                img['alt'] = tex.text
+                img['class'] = tex.get('class')
+                tex.replace_with(img)
+                if options.verbose:
+                    print("Modified formula %s to: %s" % (tex, img))
+
+            url = baseUrl + '%s/pages' % (course_id)
+            print("Should post page to", url)
+            payload={
+                'wiki_page[title]': data['title'],
+                'wiki_page[published]': False,
+                'wiki_page[body]': str(html)
+            }
             if options.verbose:
-                print("result of post creating page: %s" % page_response)
-            print("Uploaded page to %s" % page_response['html_url'])
-            data['url'] = page_response['html_url']
-        else:
-            print("Failed to upload page %s" % data['title'])
-    dumpname = '%s/%s/zzz-import-%s-%s.json' % (
-        dumpdir, course_code, course_code, datetime.now().strftime('%Y%m%d-%H%M%S'))
-    with open(dumpname, 'w') as json:
-        dump_json(dumpdata, json, indent=4)
-    result = create_file(course_id, dumpname, basename(dumpname))
-    print('Uploaded final result to %s' % result)
+                print(payload)
+            r = requests.post(url, headers = header, data=payload)
+            if r.status_code == requests.codes.ok:
+                page_response=r.json()
+                if options.verbose:
+                    print("result of post creating page: %s" % page_response)
+                print("Uploaded page to %s" % page_response['html_url'])
+                data['url'] = page_response['html_url']
+            else:
+                print("Failed to upload page %s: %s" % (data['title'], r))
+        dumpname = '%s/%s/zzz-import-%s-%s.json' % (
+            dumpdir, course_code, course_code, datetime.now().strftime('%Y%m%d-%H%M%S'))
+        with open(dumpname, 'w') as json:
+            dump_json(dumpdata, json, indent=4)
+        result = create_file(course_id, dumpname, basename(dumpname))
+        print('Uploaded final result to %s' % result)
 
 def create_file(course_id, full_folder_name, file_name, verbose=False):
     url = baseUrl + '%s/files' %(course_id)
-    statinfo = stat(full_folder_name)
+    try:
+        statinfo = stat(full_folder_name)
+    except:
+        from urllib.parse import unquote
+        full_folder_name = unquote(full_folder_name)
+        statinfo = stat(full_folder_name)
     payload = {
         'name' :  file_name,
         'size' :  statinfo.st_size,
@@ -170,7 +178,7 @@ def create_file(course_id, full_folder_name, file_name, verbose=False):
     url = phase3_data['url']
     return url[0:url.find('?')]
 
-def find_canvas_id(coursecode):
+def find_canvas_id(coursecode, forterm='VT17'):
     print('Url: %s' % ('%s/courses/%s' % (lmsapiurl, coursecode[:6])))
     resp = requests.get('%s/courses/%s' % (lmsapiurl, coursecode[:6]))
     if resp.status_code != 200:
@@ -179,12 +187,18 @@ def find_canvas_id(coursecode):
     data = resp.json()
     if len(data) == 1:
         return data[0]['id']
+    found = {}
     for j in data:
         if j['sis_course_id'] == coursecode:
             return j['id']
+        if j['sis_course_id'][:10] == coursecode + forterm:
+            found[j['sis_course_id']] = j['id']
         #else:
         #    print('Ignoring %s' % j['sis_course_id'])
-    print('Failed to get canvas data for %s' % coursecode);
+    if len(found) == 1:
+        print("This should be simple: %s" % found)
+        return found.popitem()[1]
+    print('Failed to get canvas data for %s; got: %s' % (coursecode, data));
     return None
 
 
